aa939eb2c83fe3660cddfff31d867d55d5f4aa4d
hosein ladmakhi <ladmakhi81@gmail.com>
2024-11-19 14:56:24 +0330

    feat: create the modules of the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { UserAddressModule } from './user-address/user-address.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { ProductPriceModule } from './product-price/product-price.module';
+import { UserCartModule } from './user-cart/user-cart.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { ReturnedOrderModule } from './returned-order/returned-order.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    UserAddressModule,
+    CategoryModule,
+    ProductModule,
+    ProductPriceModule,
+    UserCartModule,
+    OrderModule,
+    PaymentModule,
+    TransactionModule,
+    ReturnedOrderModule,
+    WishlistModule,
+    HistoryModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('APP_PORT'));
+
+  app.enableCors();
+
+  await app.listen(port, () => {
+    console.log(`the server is running: ${app.getUrl()}`);
+  });
+};
+
+export default bootstrap();
